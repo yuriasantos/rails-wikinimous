dafc69ac66f81115bd832dd300312bf55ddbb5aa
Yuri Santos <santosyurial@gmail.com>
2024-02-22 18:22:36 -0300

    Implement CRUD methods in controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+(1..10).to_a.each do |_|
+  Article.create(title: Faker::Book.title, content: Faker::Lorem.paragraph)
+end
